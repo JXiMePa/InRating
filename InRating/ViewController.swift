55c87da796a7016f6123834f18d772e0c15faabb
Юрій Тарасенко <jurikfav0rit@gmail.com>
2019-01-22 12:13:09 +0200

    Initial Commit

---
+++ InRating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InRating
+//
+//  Created by Tarasenko Jurik on 1/22/19.
+//  Copyright © 2019 Next Level. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
